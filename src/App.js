b83b76ee7c62e3bc6ab79c9917a0b2540618bbe5
FajrNaveed5197 <fajrnaveed5197@gmail.com>
2023-09-07 09:56:56 +0500

    last

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import axios from "axios";
 
@@ -21,12 +22,18 @@ const App = () => {
   }, []); // Empty dependency array means this effect runs once after the initial render
 
   return (
-    <div className="container">
-      <div className="card">
-        <h1 className="heading">{advice}</h1>
-        <button className="button" onClick={fetchAdvice}>
-          <span>New Advice</span>
-        </button>
+    <div className="backgroundhai">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-5 col-sm-8" style={{display:"flex", alignItems:"center"}}>
+            <div className="card" style={{display:"flex", alignItems:"center", height:"50%", borderRadius:"20px"}}>
+              <h1 className="heading">{advice}</h1>
+              <button className="button mb-3" onClick={fetchAdvice}>
+                <span>New Advice</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
